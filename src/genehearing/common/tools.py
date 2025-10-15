cd292ff7e844e4ba8f77f5c53f5e17c03f30fcfe
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-10-15 15:37:40 +0200

    Create tools.py

---
+++ src/genehearing/common/tools.py
@@ -0,0 +1,28 @@
+import yaml
+import pandas as pd
+
+def load_config():
+    # Read in the configuration file
+    with open('config.yaml') as p:
+        config = yaml.safe_load(p)
+    return config
+
+def parse_list(arg_str):
+    """Konwertuje string 'a,b,c' na listę ['a','b','c']"""
+    return [x.strip() for x in arg_str.split(',')] if arg_str else []
+
+
+def parse_map(map_str):
+    """
+    Konwertuje string 'Arkusz1=klucz1,Arkusz2=klucz2' na słownik
+    """
+    mapping = {}
+    for pair in map_str.split(','):
+        sheet, key = pair.split('=')
+        mapping[sheet.strip()] = key.strip()
+    return mapping
+
+def convert_to_datetime(df, column, suffix):
+    col = f"{column}_{suffix}"
+    df[col] = pd.to_datetime(df[col], errors="coerce")
+    return df
