4ffa4d32aaf1a4c03ffe57483d99bc2b80d2e5eb
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-10-30 14:52:49 +0100

    Update preprocessing_core.py

--- src/genehearing/preprocess/objects/preprocessing_core.py
+++ src/genehearing/preprocess/objects/preprocessing_core.py
@@ -14,6 +14,7 @@ class CSVProcessor:
         
     def read_merge_genetic_audiometry(self):
         self.merged = pd.merge(self.data_audiometry, self.data_genetic, how='left', on=self.match_column)
+        self.merged = self.merged.loc[:, ~self.merged.columns.str.contains('^Unnamed')]
 
 
     def save_merged(self):
