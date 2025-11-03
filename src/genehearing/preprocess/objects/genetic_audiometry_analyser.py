b6dabf0f5b99bb7c371f71f2850eae91c5109e96
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-11-03 12:44:45 +0100

    choose first examinations

--- src/genehearing/preprocess/objects/genetic_audiometry_analyser.py
+++ src/genehearing/preprocess/objects/genetic_audiometry_analyser.py
@@ -8,3 +8,14 @@ class GenehearingAnalyser(TonalAudiometry):
                   air_audiometry=["AirMask", "Air"],
                   bone_audiometry=["BoneMask", "Bone"]):
             super().__init__(path, tonal_suffix, columnnames, air_audiometry, bone_audiometry)
+
+        def choose_first_examination(self):
+            id = 0
+            for i, mini_df in enumerate(self.mini_dfs):
+                id_new = mini_df[self.patient_number_columnname].values[0]
+                print(id, id_new)
+                if id_new == id:
+                    self.mini_dfs[i]['IF_FIRST'] = 0
+                else:
+                    self.mini_dfs[i]['IF_FIRST'] = 1
+                id = id_new
