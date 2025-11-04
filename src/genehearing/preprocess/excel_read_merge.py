01c1c0c1f01bcb6040d4a29818abea7589d6c21e
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-11-04 15:23:33 +0100

    formating fix

--- src/genehearing/preprocess/excel_read_merge.py
+++ src/genehearing/preprocess/excel_read_merge.py
@@ -11,7 +11,9 @@ def main():
     genetic_datapath = config["datarawdirectory"] + config['genetic_dataname_all'] + '.csv'
     
 
-    tonal_audiometry_processor = CSVProcessor(tonal_audiometry_datapath, tonal_suffix, genetic_datapath, output_path=config["datarawdirectory"], match_column=config['pesel_columnname']) 
+    tonal_audiometry_processor = CSVProcessor(tonal_audiometry_datapath, tonal_suffix, genetic_datapath,
+                                              output_path=config["datarawdirectory"], 
+                                              match_column=config['pesel_columnname']) 
     tonal_audiometry_processor.read_merge_genetic_audiometry()
     tonal_audiometry_processor.save_merged()
 
