502d41ce92753692fd7e27583f92ef83bc76f45d
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2025-12-22 14:22:02 +0100

    PTA_columns

--- src/genehearing/preprocess/preprocess_audiometry.py
+++ src/genehearing/preprocess/preprocess_audiometry.py
@@ -59,7 +59,7 @@ def main():
     
     tonal_audiometry_processor.calculate_mean_ear_pta(PTA_columns)
 
-    tonal_audiometry_processor.classificate_hearing_loss(config["biap_hearing_levels"], config["asha_hearing_levels"])
+    tonal_audiometry_processor.classificate_hearing_loss(PTA_columns, config["biap_hearing_levels"], config["asha_hearing_levels"])
     tonal_audiometry_processor.match_audiogram_type(config['audiogram_types_criteria_zone_1'], config['audiogram_types_criteria_zone_2'],
                                                     config['audiogram_types_criteria_zones'])
 
