20b1dfd48bf64c54abdd2538c9202ef5ce99448b
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2026-01-14 15:46:59 +0100

    Update excel_read_merge.py

--- src/genehearing/preprocess/excel_read_merge.py
+++ src/genehearing/preprocess/excel_read_merge.py
@@ -7,14 +7,16 @@ def main():
     config = tools.load_config()
     tonaldataname=config["tonaldataname"]
     tonal_suffix = tonaldataname.split("_")[-1]
-    tonal_audiometry_datapath = config["datarawdirectory"] + tonaldataname + '.csv'
+    tonal_audiometry_datapath = config["datarawdirectory"] + tonaldataname + '_final'+'.csv'
     genetic_datapath = config["datarawdirectory"] + config['genetic_dataname_all'] + '.csv'
-    
+    abr_datapath = config["datarawdirectory"] + config['abr'] + '.csv'
 
     tonal_audiometry_processor = CSVProcessor(tonal_audiometry_datapath, tonal_suffix, genetic_datapath,
+                                              abr_datapath,
                                               output_path=config["datarawdirectory"], 
                                               match_column=config['pesel_columnname']) 
     tonal_audiometry_processor.read_merge_genetic_audiometry()
+    tonal_audiometry_processor.merge_abr()
     tonal_audiometry_processor.decode_sex()
     tonal_audiometry_processor.save_merged()
 
