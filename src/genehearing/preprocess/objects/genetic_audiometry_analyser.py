4c46963a3f83811832f581e80c40da6bf1f282bb
Maria Magdalena Waligórska <73756836+maria0026@users.noreply.github.com>
2026-01-17 21:54:45 +0100

    update exporting data

--- src/genehearing/preprocess/objects/genetic_audiometry_analyser.py
+++ src/genehearing/preprocess/objects/genetic_audiometry_analyser.py
@@ -25,7 +25,11 @@ class GenehearingAnalyser(TonalAudiometry):
                 id = id_new
 
 
-        def create_dataframe_for_merging(self, output_path):
+        def create_dataframe_for_merging(self, biap_columns, output_path):
+            biap_names = []
+            for ear in self.ears:
+                for biap_column in biap_columns:
+                    biap_names.append(f"{ear}_{biap_column}")
             for i, mini_df in enumerate(self.mini_dfs):
                 ears_grouped = {g: d for g, d in mini_df.groupby("EAR_SIDE")}
                 for ear in self.ears:
@@ -33,14 +37,14 @@ class GenehearingAnalyser(TonalAudiometry):
                         continue
 
                     group = ears_grouped[ear]
-                    biap_values = group['BIAP'].dropna()
-                    if biap_values.empty:
-                        self.mini_dfs[i][f'{ear}_BIAP'] = None
-                    else:
-                        self.mini_dfs[i][f'{ear}_BIAP'] = biap_values.iloc[0]
+                    for biap_column in biap_columns:
+                        biap_values = group[biap_column].dropna()
+                        if biap_values.empty:
+                            self.mini_dfs[i][f'{ear}_{biap_column}'] = None
+                        else:
+                            self.mini_dfs[i][f'{ear}_{biap_column}'] = biap_values.iloc[0]
 
                     hearing_type_vals = group['HEARING_TYPE'].dropna()
-
                     if hearing_type_vals.empty:
                         self.mini_dfs[i][f'{ear}_HEARING_TYPE'] = None
                     else:
@@ -52,16 +56,16 @@ class GenehearingAnalyser(TonalAudiometry):
 
             merged_df = pd.concat(self.mini_dfs, ignore_index=True)
             merged_df[self.date_column] = merged_df[self.date_column].dt.strftime("%d.%m.%Y %H:%M")
-            self.new_df = merged_df.loc[:, [self.patient_number_columnname, self.date_column, 'IF_FIRST', 'L_BIAP', 'P_BIAP', 'L_HEARING_TYPE', 'P_HEARING_TYPE', 'PATID']]
+            self.new_df = merged_df.loc[:, [self.patient_number_columnname, self.date_column, 'IF_FIRST', *biap_names, 'L_HEARING_TYPE', 'P_HEARING_TYPE']]
             if not os.path.exists(output_path):
                 os.makedirs(output_path)
             self.new_df.to_csv(f'{output_path}audiometry_{self.tonal_suffix}_summarized.csv', index=False)
             print(f'Saving to {output_path}audiometry_{self.tonal_suffix}_summarized.csv completed.')
 
 
-        def create_disinct_datasets(self, hearing_loss_dict, output_path):
+
+        def create_disinct_datasets(self, hearing_loss_dict, biap_columns, output_path):
             for loss_type, rules in hearing_loss_dict.items():
-    
-                df_filtered = self.new_df[( ((self.new_df['L_BIAP']==rules[0]) | (self.new_df['P_BIAP']==rules[1])) | ((self.new_df['L_BIAP']==rules[1]) | (self.new_df['P_BIAP']==rules[0])) )]                
+                df_filtered = self.new_df[(self.new_df[biap_columns[0]]==rules[0]) & (self.new_df[biap_columns[1]]==rules[1])]
                 df_filtered.to_csv(f'{output_path}audiometry_{loss_type}.csv', index=False)
                 print(f'Saving to {output_path}audiometry_{loss_type}.csv completed.')
